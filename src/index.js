1f891b56ac29e3db0251503bff2df63dbb59103a
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-09-18 23:38:36 +0200

    Basic setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import StringValidator from './validators/StringValidator.js'
+
+const stringValidator = new StringValidator()
+stringValidator.print()
+stringValidator.prints('Input with Valid String')
---
+++ src/validators/StringValidator.js
@@ -0,0 +1,12 @@
+export default class StringValidator {
+  constructor() {
+    this.message = 'Message with Valid String'
+  }
+
+  print() {
+    console.log(this.message)
+  }
+  prints(text) {
+    console.log(text)
+  }
+}
